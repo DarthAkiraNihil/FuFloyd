32d7e49daf871db2250dac43c1117fd029aff247
TheSwagVader <egorzverev666@gmail.com>
2023-05-15 11:50:32 +0300

    Improved interface

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ const int MAIN_DIAG_MARK = -1000000;
 
 #define min(x, y) (x < y) ? x : y
 #define DrawMainFrame drawFrame(78, 30, 2, 2)
-#define PrintNullLine printf("                                                       ")
+#define PrintNullLine printf("                                                           ")
 
 void prepareMatrix(float** graph, int pointsCount) {
     for (int i = 0; i < pointsCount; i++) {
@@ -47,19 +47,8 @@ void floydAlgorithm(float** graph, int pointsCount) {
                     }
                     else graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j]);
                 }
-
-
             }
         }
-    }
-
-}
-
-void makeNullMatrix(float** graph, int pointsCount) {
-    graph = new float* [pointsCount];
-    for (int i = 0; i < pointsCount; i++) {
-        graph[i] = new float[pointsCount];
-        //memset(graph[i], 0, pointsCount * sizeof(float));
     }
 }
 
@@ -83,174 +72,207 @@ void printAdjacencyMatrix(float** graph, int pointsCount, int ltcX, int ltcY) {
     }
 }
 
-void writeAdjacencyMatrixToFile(float** graph, int pointsCount) {
-    time_t sc = time(nullptr);
-    char outputFile[255]; sprintf(outputFile, "graph-%lld.txt", (long long) sc);
-    outputFile[strlen(outputFile)] = 0;
-    FILE* out = fopen(outputFile, "w+");
+void writeAdjacencyMatrixToFile(float** graph, int pointsCount, FILE* file, bool afterFloyd = false) {
+    fprintf(file, (afterFloyd) ? "Матрица, после применения алгоритма Флойда\n" : "Весовая матрица введённого графа:\n");
     for (int i = 0; i < pointsCount; i++) {
         for (int j = 0; j < pointsCount; j++) {
-            //gotoxy(ltcX + j * 7, ltcY + i);
             if (graph[i][j] == INFINITY) {
-                fprintf(out, "[   INF   ]");
+                fprintf(file, "[   INF   ]");
             }
             else if (graph[i][j] == MAIN_DIAG_MARK) {
-                fprintf(out, "[    X    ]");
+                fprintf(file, "[    X    ]");
             }
             else {
-                fprintf(out, "[%9.4f]", graph[i][j]);
-                /*printf("[");
-                printf("%5.2f", graph[i][j]);
-                gotoxy(ltcX + j * 7 + 6, ltcY + i); printf("]");*/
+                fprintf(file, "[%9.3f]", graph[i][j]);
             }
         }
-        fprintf(out, "\n");
-    }
-    fclose(out);
-}
-
-void inputMatrix() {
-    //makeNullMatrix(graph, pointsCount);
-    DrawMainFrame;
-
-    int pointsCount = 0;
-    do {
-        gotoxy(4, 4); printf("Введите количество вершин в графе (маск. 8): ");
-        scanf("%d", &pointsCount);
-        if (pointsCount < 1 || pointsCount > 8) {
-            gotoxy(4, 5); printf("Неверно введено количество вершин графа.\n");
-            gotoxy(4, 6); printf("Пожалуйста, попробуйте ещё раз.\n");
-            gotoxy(4, 7); printf("Нажмите любую клавишу для продолжения");
-            getchar(); getchar();
-            gotoxy(4, 5); PrintNullLine;
-            gotoxy(4, 6); PrintNullLine;
-            gotoxy(4, 7); PrintNullLine;
-        }
-    } while (pointsCount < 1 || pointsCount > 8);
-    //makeNullMatrix(graph, pointsCount)
-    // ;
-    float** adjacencyMatrix = new float* [pointsCount];
-    for (int i = 0; i < pointsCount; i++) {
-        adjacencyMatrix[i] = new float [pointsCount];
-    }
-    gotoxy(4, 5); printf("Переходим к заполнению матрицы смежности графа");
-    for (int i = 0; i < pointsCount; i++) {
-        for (int j = 0; j < pointsCount; j++) {
-            gotoxy(4, 6 + j);
-            printf("Введите элемент A[%d][%d]: ", i + 1, j + 1);
-            //gotoxy(29, 6 + j);
-            if (i == j) {
-                printf("[ЭЛЕМЕНТ ГЛАВНОЙ ДИАГОНАЛИ]");
-                adjacencyMatrix[i][j] = MAIN_DIAG_MARK;
-            }
-            else {
-                scanf("%f", &adjacencyMatrix[i][j]);
-            }
-        }
-        for (int j = 0; j < pointsCount; j++) {
-            gotoxy(4, 6 + j); PrintNullLine;
-        }
-    }
-    gotoxy(4, 5); printf("Матрица смежности графа успешно заполнена.    "); getchar();
-    bool stepByStep = false; int outputMode;
-    gotoxy(4, 6); printf("Куда вывести итоговую матрицу?");
-    do {
-        gotoxy(4, 7); printf("1 - в файл (повышенная точность вывода), 2 - в консоли: ");
-        scanf("%d", &outputMode);
-        if (outputMode != 1 && outputMode != 2) {
-
-            gotoxy(4, 8); printf("Неверно введён режим вывода\n");
-            gotoxy(4, 9); printf("Пожалуйста, попробуйте ещё раз.\n");
-            gotoxy(4, 10); printf("Нажмите любую клавишу для продолжения");
-            getchar(); getchar();
-            gotoxy(4, 8); PrintNullLine;
-            gotoxy(4, 9); PrintNullLine;
-            gotoxy(4, 10); PrintNullLine;
-        }
-    } while (outputMode != 1 && outputMode != 2);
-    //printf("%.2f", adjacencyMatrix[0][1]);
-    floydAlgorithm(adjacencyMatrix, pointsCount);
-    switch (outputMode) {
-        case 1: {
-            writeAdjacencyMatrixToFile(adjacencyMatrix, pointsCount);
-            break;
-        }
-        case 2: {
-            printAdjacencyMatrix(adjacencyMatrix, pointsCount, 4, 10);
-            break;
-        }
-    }
-    getchar();
-
-
-
-    //getchar(); getchar();
-
-    for (int i = 0; i < pointsCount; i++) delete [] adjacencyMatrix[i];
-    delete [] adjacencyMatrix;
-}
-
-void loadMatrixFromFile() {
-    DrawMainFrame;
-
-    int pointsCount = 0;
-    char graphFileName[255];
-    float** adjacencyMatrix;
-    gotoxy(4, 4); printf("Введите имя файла графа: ");
-    fgets(graphFileName, 255, stdin);
-    graphFileName[strlen(graphFileName) - 1] = 0;
-    FILE* graphFile = fopen(graphFileName, "r");
-    if (graphFile != nullptr) {
-        fscanf(graphFile, "%d", &pointsCount);
-        adjacencyMatrix = new float* [pointsCount];
-        for (int i = 0; i < pointsCount; i++) {
-            adjacencyMatrix[i] = new float [pointsCount];
-        }
-        char line[512]; line[0] = 0;
-        fgets(line, 512, graphFile);
-        for (int i = 0; i < pointsCount; i++) {
-            int index = 0;
-            fgets(line, 512, graphFile);
-            char * tokenPointer = strtok(line, " ");
-            while (tokenPointer != nullptr) {
-                adjacencyMatrix[i][index++] = (float) atof(tokenPointer);
-                //puts(tokenPointer);
-                tokenPointer = strtok(nullptr, " ");
-            }
-            //puts(line);
-        }
-        floydAlgorithm(adjacencyMatrix, pointsCount);
-        writeAdjacencyMatrixToFile(adjacencyMatrix, pointsCount);
-        //printAdjacencyMatrix(adjacencyMatrix, pointsCount, 4, 15);
-        for (int i = 0; i < pointsCount; i++) delete [] adjacencyMatrix[i];
-        delete [] adjacencyMatrix;
-        //getchar(); getchar();
+        fprintf(file, "\n");
 
     }
-
-
+    fprintf(file, "\n");
 }
 
 int main() {
     setWindowSize(WINDOW_SIZE_WIDTH, WINDOW_SIZE_HEIGHT);
     hideCursor();
     drawFrame(78, 30, 2, 2);
-
     setCP(1251);
     float** adjacencyMatrix;
     int choice = 1;
     do {
+        DrawMainFrame;
         choice = drawMenu(choice, mainMenu, 4, 5, 12, 4, 2, 1);
         switch (choice) {
             case 1: {
-                inputMatrix();
-                //printf("%f", adjacencyMatrix[0][1]);
-                //getchar();
-
+                DrawMainFrame;
+                int pointsCount = 0;
+                do {
+                    gotoxy(4, 4); printf("Введите количество вершин в графе (маск. 8): ");
+                    scanf("%d", &pointsCount);
+                    if (pointsCount < 1 || pointsCount > 8) {
+                        gotoxy(4, 5); printf("Неверно введено количество вершин графа.\n");
+                        gotoxy(4, 6); printf("Пожалуйста, попробуйте ещё раз.\n");
+                        gotoxy(4, 7); printf("Нажмите любую клавишу для продолжения");
+                        getchar(); getchar();
+                        gotoxy(4, 5); PrintNullLine;
+                        gotoxy(4, 6); PrintNullLine;
+                        gotoxy(4, 7); PrintNullLine;
+                        gotoxy(4, 4); PrintNullLine;
+                    }
+                } while (pointsCount < 1 || pointsCount > 8);
+                adjacencyMatrix = new float* [pointsCount];
+                for (int i = 0; i < pointsCount; i++) {
+                    adjacencyMatrix[i] = new float [pointsCount];
+                }
+                gotoxy(4, 5); printf("Переходим к заполнению весовой матрицы графа");
+                for (int i = 0; i < pointsCount; i++) {
+                    for (int j = 0; j < pointsCount; j++) {
+                        gotoxy(4, 6 + j);
+                        printf("Введите элемент V[%d][%d]: ", i + 1, j + 1);
+                        //gotoxy(29, 6 + j);
+                        if (i == j) {
+                            printf("[ЭЛЕМЕНТ ГЛАВНОЙ ДИАГОНАЛИ]");
+                            adjacencyMatrix[i][j] = MAIN_DIAG_MARK;
+                        }
+                        else {
+                            scanf("%f", &adjacencyMatrix[i][j]);
+                        }
+                    }
+                    for (int j = 0; j < pointsCount; j++) {
+                        gotoxy(4, 6 + j); PrintNullLine;
+                    }
+                }
+                gotoxy(4, 5); printf("Весовая матрица графа успешно заполнена.    "); getchar();
+                int outputMode;
+                gotoxy(4, 6); printf("Куда вывести итоговую матрицу?");
+                do {
+                    gotoxy(4, 7); printf("1 - в файл (повышенная точность вывода), 2 - в консоли: ");
+                    scanf("%d", &outputMode);
+                    if (outputMode != 1 && outputMode != 2) {
+                        gotoxy(4, 8); printf("Неверно введён режим вывода\n");
+                        gotoxy(4, 9); printf("Пожалуйста, попробуйте ещё раз.\n");
+                        gotoxy(4, 10); printf("Нажмите любую клавишу для продолжения");
+                        waitForKey(13);
+                        //getchar(); getchar();
+                        gotoxy(4, 8); PrintNullLine;
+                        gotoxy(4, 9); PrintNullLine;
+                        gotoxy(4, 10); PrintNullLine;
+                        gotoxy(4, 7); PrintNullLine;
+                    }
+                } while (outputMode != 1 && outputMode != 2);
+                gotoxy(4, 6); PrintNullLine;
+                gotoxy(4, 7); PrintNullLine;
+                switch (outputMode) {
+                    case 1: {
+                        time_t sc = time(nullptr);
+                        char outputFile[255]; sprintf(outputFile, "graph-%lld.txt", (long long) sc);
+                        outputFile[strlen(outputFile)] = 0;
+                        FILE* out = fopen(outputFile, "w+");
+                        writeAdjacencyMatrixToFile(adjacencyMatrix, pointsCount, out);
+                        floydAlgorithm(adjacencyMatrix, pointsCount);
+                        writeAdjacencyMatrixToFile(adjacencyMatrix, pointsCount, out, true);
+                        gotoxy(4, 6); printf("Матрица Флойда записана в файл %s", outputFile);
+                        gotoxy(4, 7); printf("Нажмите любую клавишу для продолжения");
+                        waitForKey(13);
+                        fclose(out);
+                        break;
+                    }
+                    case 2: {
+                        int stepByStep;
+                        do {
+                            gotoxy(4, 6); printf("Выполнять алгоритм пошагово? (1 - да, 0 - нет) ");
+                            scanf("%d", &stepByStep);
+                            if (stepByStep != 1 && stepByStep != 0) {
+                                gotoxy(4, 7); printf("Неверно введён режим отображения выполнения\n");
+                                gotoxy(4, 8); printf("Пожалуйста, попробуйте ещё раз.\n");
+                                gotoxy(4, 9); printf("Нажмите любую клавишу для продолжения");
+                                waitForKey(13);
+                                gotoxy(4, 7); PrintNullLine;
+                                gotoxy(4, 8); PrintNullLine;
+                                gotoxy(4, 9); PrintNullLine;
+                                gotoxy(4, 6); PrintNullLine;
+                            }
+                            gotoxy(4, 6); PrintNullLine;
+                        } while(stepByStep != 1 && stepByStep != 0);
+                        switch (stepByStep) {
+                            case 1: {
+                                prepareMatrix(adjacencyMatrix, pointsCount);
+                                gotoxy(4, 6); printf("Выполнение алгоритма Флойда пошагово");
+                                for (int k = 0; k < pointsCount; k++) {
+                                    floydAlgorithmOneStep(adjacencyMatrix, pointsCount, k);
+                                    gotoxy(4, 7); printf("Итерация %d/%d", k + 1, pointsCount);
+                                    printAdjacencyMatrix(adjacencyMatrix, pointsCount, 4, 9);
+                                    gotoxy(4, 10 + pointsCount); printf("Enter = Следующая итерация");
+                                    waitForKey(13);
+                                }
+                                gotoxy(4, 7); printf("Матрица, после выполнения алгоритма Флойда:");
+                                gotoxy(4, 10 + pointsCount); printf("Нажмите Enter для продолжения");
+                                waitForKey(13);
+                                break;
+                            }
+                            case 0: {
+                                floydAlgorithm(adjacencyMatrix, pointsCount);
+                                gotoxy(4, 7); printf("Матрица, после выполнения алгоритма Флойда:");
+                                printAdjacencyMatrix(adjacencyMatrix, pointsCount, 4, 9);
+                                gotoxy(4, 10 + pointsCount); printf("Нажмите Enter для продолжения");
+                                waitForKey(13);
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                }
+                for (int i = 0; i < pointsCount; i++) delete [] adjacencyMatrix[i];
+                delete [] adjacencyMatrix;
                 break;
             }
             case 2: {
-                loadMatrixFromFile();
+                DrawMainFrame;
+                int pointsCount = 0;
+                char graphFileName[255];
+                gotoxy(4, 4); printf("Введите имя файла графа: ");
+                fgets(graphFileName, 255, stdin);
+                graphFileName[strlen(graphFileName) - 1] = 0;
+                FILE* graphFile = fopen(graphFileName, "r");
+                if (graphFile != nullptr) {
+                    fscanf(graphFile, "%d", &pointsCount);
+                    adjacencyMatrix = new float* [pointsCount];
+                    for (int i = 0; i < pointsCount; i++) {
+                        adjacencyMatrix[i] = new float [pointsCount];
+                    }
+                    char line[512]; line[0] = 0;
+                    fgets(line, 512, graphFile);
+                    for (int i = 0; i < pointsCount; i++) {
+                        int index = 0;
+                        fgets(line, 512, graphFile);
+                        char * tokenPointer = strtok(line, " ");
+                        while (tokenPointer != nullptr) {
+                            adjacencyMatrix[i][index++] = (float) atof(tokenPointer);
+                            tokenPointer = strtok(nullptr, " ");
+                        }
+                    }
+
+                    time_t sc = time(nullptr);
+                    char outputFile[255]; sprintf(outputFile, "graph-%lld.txt", (long long) sc);
+                    outputFile[strlen(outputFile)] = 0;
+                    FILE* out = fopen(outputFile, "w+");
+
+                    writeAdjacencyMatrixToFile(adjacencyMatrix, pointsCount, out);
+                    floydAlgorithm(adjacencyMatrix, pointsCount);
+                    writeAdjacencyMatrixToFile(adjacencyMatrix, pointsCount, out, true);
+                    gotoxy(4, 5); printf("Матрица Флойда записана в файл %s", outputFile);
+                    gotoxy(4, 6); printf("Нажмите Enter для продолжения");
+                    waitForKey(13);
+                    fclose(out);
+                    fclose(out);
+                    for (int i = 0; i < pointsCount; i++) delete [] adjacencyMatrix[i];
+                    delete [] adjacencyMatrix;
+                }
+                else {
+                    gotoxy(4, 5); printf("Такого файла не существует");
+                    gotoxy(4, 6); printf("Нажмите Enter для продолжения");
+                    waitForKey(13);
+                }
                 break;
             }
             case 3: {
@@ -262,30 +284,4 @@ int main() {
             }
         }
     } while (choice != 4);
-    /*int pointsCount; scanf("%d", &pointsCount);
-    float** adjustmentMatrix = new float*[pointsCount];
-    for (int i = 0; i < pointsCount; i++) {
-        adjustmentMatrix[i] = new float [pointsCount];
-        //for (int j = 0; j < pointsCount; j++) scanf("%f", &adjustmentMatrix[i][j]);
-    }
-    inputGraphMatrix(adjustmentMatrix, pointsCount);
-    floydAlgorithm(adjustmentMatrix, pointsCount);
-    for (int i = 0; i < pointsCount; i++) {
-        for (int j = 0; j < pointsCount; j++) {
-            if (adjustmentMatrix[i][j] == INFINITY) {
-                printf("INF ");
-            }
-            else if (adjustmentMatrix[i][j] == MAIN_DIAG_MARK) {
-                printf("X ");
-            }
-            else {
-                printf("%f ", adjustmentMatrix[i][j]);
-            }
-
-        }
-        printf("\n");
-    }
-    getchar(); getchar(); getchar(); getchar(); getchar();*/
-    //std::cout << "Hello, World!" << std::endl;
-    //return 0;
 }
